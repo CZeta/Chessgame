00573bff02109910f9d6e48580d5e5b269dd82ad
Jan <janh93@hotmail.de>
2021-02-28 14:47:21 +0100

    A class for all pieces

--- Chess/src/Figure.java
+++ Chess/src/Figure.java
@@ -1,24 +1,45 @@
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-abstract class Figure {
-int[] position=new int[2];
-int Farbe;
-static BufferedImage[] bild=new BufferedImage[2];
+import javax.imageio.ImageIO;
 
-public abstract boolean ValidMove(int[] a);
-public abstract boolean Check(int[] KingsPos);
-
-public void Move(int[] a) {
-	if (ValidMove(a)) {
-		position[0]=a[0];
-		position[1]=a[1];
+public abstract class Figure {
+	
+	int[] position=new int[2];
+	int Farbe;
+	BufferedImage bild;
+	String[] FileName=new String[2];
+	
+	
+	public Figure(int[] position, int farbe) {
+		this.position=position;
+		this.Farbe=farbe;
+		
 	}
-}
-public void geschlagen() {
-	bild[0]=null;
-	bild[1]=null;
-	position[0]=99;
-	position[1]=99;
-}
-
+	
+	public void initPic() {
+		try {
+			bild=ImageIO.read(new File(FileName[Farbe]));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public abstract boolean ValidMove(int[] a);
+	
+	public void Move(int[]a) {
+		if(ValidMove(a)) {
+			position[0]=a[0];
+			position[1]=a[1];
+		}
+	}
+	
+	public void geschlagen() {
+		
+	}
+	
+	public abstract boolean Check(int[] KingsPos);
+	
 }
